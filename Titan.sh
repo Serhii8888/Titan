ea481136f0d2bbdaa83df8732b8efc88f242aade
Serhii8888 <137101635+Serhii8888@users.noreply.github.com>
2025-06-03 13:42:06 +0300

    Create Titan.sh

---
+++ Titan.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+channel_logo() {
+    echo -e "\n\nПідпишись на найкращий канал про ноди — @NodeUA [💸]"
+}
+
+download_node() {
+  if [ -d "$HOME/.titanedge" ]; then
+    echo "Папка .titanedge вже існує. Видаліть ноду та встановіть заново. Вихід..."
+    return 0
+  fi
+
+  sudo apt install lsof
+
+  ports=(1234 55702 48710)
+
+  for port in "${ports[@]}"; do
+    if [[ $(lsof -i :"$port" | wc -l) -gt 0 ]]; then
+      echo "Помилка: Порт $port зайнятий. Програма не зможе виконатись."
+      exit 1
+    fi
+  done
+
+  echo -e "Всі порти вільні! Починається встановлення...\n"
+
+  echo 'Починаю встановлення...'
+
+  cd $HOME
+
+  sudo apt update -y && sudo apt upgrade -y
+  sudo apt-get install nano git gnupg lsb-release apt-transport-https jq screen ca-certificates curl -y
+
+  if ! command -v docker &> /dev/null; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    rm get-docker.sh
+  else
+    echo "Docker вже встановлений. Пропускаємо."
+  fi
+
+  if ! command -v docker-compose &> /dev/null; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+  else
+    echo "Docker-Compose вже встановлений. Пропускаємо."
+  fi
+
+  echo 'Усі необхідні залежності встановлені. Запустіть ноду за допомогою пункту 2.'
+}
+
+launch_node() {
+  docker ps -a --filter "ancestor=nezha123/titan-edge" --format "{{.ID}}" | while read container_id; do
+    docker stop "$container_id"
+    docker rm "$container_id"
+  done
+
+  while true; do
+    echo -en "Введіть ваш HASH: "
+    read HASH
+    if [ ! -z "$HASH" ]; then
+        break
+    fi
+    echo 'HASH не може бути порожнім.'
+  done
+
+  docker run --network=host -d -v ~/.titanedge:$HOME/.titanedge nezha123/titan-edge
+  sleep 10
+
+  docker run --rm -it -v ~/.titanedge:$HOME/.titanedge nezha123/titan-edge bind --hash=$HASH https://api-test1.container1.titannet.io/api/v2/device/binding
+
+  echo -e "Ноду запущено."
+}
